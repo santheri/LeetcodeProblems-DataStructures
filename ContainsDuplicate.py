465a4eb8ca683c5bf87e70a513e3ab57c60c11c2
Santheri Bhat <santheribhat@gmail.com>
2023-01-06 17:48:07 +0530

    first commit

---
+++ ContainsDuplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+         numsSet =  set(nums)
+         if len(nums) == len(numsSet):
+            return False
+         return True
+l1=Solution()
+print(l1.containsDuplicate([1,1,2,3,4]))
