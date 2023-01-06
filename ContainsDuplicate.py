7eb567006498cebefdf43ce9fff2197d4942ceb2
Santheri Bhat <santheribhat@gmail.com>
2023-01-06 17:52:29 +0530

    first commit

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -1,3 +1,6 @@
+#Question Link:
+#https://leetcode.com/problems/contains-duplicate/description/?envType=study-plan&id=data-structure-i
+
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
